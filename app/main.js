399c1ca71994506a30d00cc9d9b28f80fdc40c3b
Guilherme Lima <guilherme.lima@alura.com>
2022-06-13 18:33:02 -0300

    video1.3

--- app/main.js
+++ app/main.js
@@ -1,11 +1,9 @@
 let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
-const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     console.table(livros)
-    exibirLivrosNaTela(livros)
 }

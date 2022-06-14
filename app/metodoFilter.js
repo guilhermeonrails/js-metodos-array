6e8bb49a36d9f5239eb298b9473ea66ea82ea8b9
Guilherme Lima <guilherme.lima@alura.com>
2022-06-14 10:57:11 -0300

    video3.4

---
+++ app/metodoFilter.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+}
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">

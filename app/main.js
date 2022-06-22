03c6f099bc276bfa36796b4d09f57c9fbe1bb3dd
guilhermeonrails <30351153+guilhermeonrails@users.noreply.github.com>
2022-06-22 16:06:17 -0300

    Update main.js

--- app/main.js
+++ app/main.js
@@ -5,6 +5,6 @@ getBuscarLivrosDaAPI()
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    let livrosComDesconto = aplicarDesconto(livros)
+    livros = aplicarDesconto(livros)
     exibirOsLivrosNaTela(livrosComDesconto)
 }
